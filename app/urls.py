58966d7748304b59456763761a8433509b3974dc
Job Karani <jobkarani084j@gmail.com>
2022-04-20 02:50:24 +0300

    creates profile paths

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_profile/', views.create_profile, name='create_profile'),
+    path('update_profile/<int:id>', views.update_profile, name='update_profile'),
+    path('profile/', views.profile, name='profile'),
+    path('accounts/profile/', views.profile, name='profile'),
 ]
