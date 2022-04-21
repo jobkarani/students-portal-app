2582eed4d6a57651788d5ab4a903bae64d98cf3e
Job Karani <jobkarani084j@gmail.com>
2022-04-21 09:26:14 +0300

    creates model student and studentclass and their respective forms

--- app/forms.py
+++ app/forms.py
@@ -14,3 +14,29 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['profile_photo', 'email', 'phone']
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+        exclude = ['student_reg']
+        widgets = {
+            'name'  :   forms.TextInput(attrs={'class':'form-control'}),
+            'roll'  :   forms.NumberInput(attrs={'class':'form-control'}),
+            'email'  :   forms.EmailInput(attrs={'class':'form-control'}),
+            'gender'  :   forms.Select(attrs={'class':'form-control'}),
+            'class'  :   forms.Select(attrs={'class':'form-control'}),
+            'dob'  :   forms.DateInput(attrs={'class':'form-control'}),
+        }
+
+
+class StudentClassForm(ModelForm):
+    class Meta:
+        model = StudentClass
+        exlude  =   'creation_date'
+        fields = '__all__'
+        widgets = {
+            'class_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'class_name_in_numeric':  forms.NumberInput(attrs={'class': 'form-control'}),
+            'section':  forms.TextInput(attrs={'class': 'form-control'}),
+        }
--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 class Profile(models.Model):
@@ -15,3 +17,40 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+class StudentClass(models.Model):
+    class_name              =   models.CharField(max_length=100, help_text='Eg- Third, Fouth,Sixth etc')
+    class_name_in_numeric   =   models.IntegerField(help_text='Eg- 1,2,4,5 etc') 
+    section                 =   models.CharField(max_length=10, help_text='Eg- A,B,C etc')
+    creation_date           =   models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('student_classes:class_list')
+
+    def __str__(self):
+        return "%s Section-%s"%(self.class_name, self.section)
+
+class Student(models.Model):
+    select_gender = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    )
+
+    name = models.CharField(max_length=100)
+    roll = models.IntegerField(unique=True)
+    email = models.EmailField()
+    gender = models.CharField(max_length=8, choices=select_gender)
+    student_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
+    dob= models.DateField(default=date.today())
+    regno = models.DateField(auto_now_add=True, auto_now=False)
+
+    def get_absolute_url(self):
+        return reverse('students:student_create')
+
+    def __str__(self):
+        return self.name
+
+class Results(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    grade = models.CharField(max_length=100)
+
